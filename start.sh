f6d609905e27271e621f13a7fde5f8f7f030d2ed
Bernard Opiyo <brobernard.254@gmail.com>
2021-09-17 03:31:46 +0300

    add views.py content

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-    export MOVIE_API_KEY='4fd793215300450014ee89ee14945659'
-export  SECRET_KEY='os.urandom(32)'
-export MAIL_USERNAME='otienonick70@gmail.com'
-export MAIL_PASSWORD='0718849600'
+
+export  SECRET_KEY='sectret2'
+export MAIL_USERNAME='brobernard.254@gmail.com'
+export MAIL_PASSWORD='BRO@2030'
 python3.8 manage.py server
