8a144552178ee1bd826bb5088324cdf6937be5ea
Bernard Opiyo <brobernard.254@gmail.com>
2021-09-19 03:58:16 +0300

    add pitch html

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,6 @@
+from app.models import Pitch
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField, SelectField
 from wtforms.validators import Required
 
 class ReviewForm(FlaskForm):
@@ -11,3 +12,27 @@ class ReviewForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    Pitch = TextAreaField('Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('product', 'product'), ('idea', 'idea'), ('business', 'business')],
+                           validators=[Required()])
+    submit = SubmitField('Pitch')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Pitch')
+
+
+class Vote(FlaskForm):
+    submit = SelectField('Like')  
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    Pitch = TextAreaField('Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('product', 'product'), ('idea', 'idea'), ('business', 'business')],
+                           validators=[Required()])
+    submit = SubmitField('Pitch')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from .forms import ReviewForm, UpdateProfile
-from ..models import User
+from ..models import Pitch, User
+from .forms import PitchForm, CommentForm, UpdateProfile
+from ..models import Pitch, Comment, User, Upvote, Downvote
 from flask_login import login_required,current_user
 from .. import db,photos
 import markdown2  
@@ -38,7 +40,7 @@ def profile(uname):
     return render_template("profile/profile.html", user = user)
 
 
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@main.route('/user/<uname>/update',methods = ['GET','Pitch'])
 def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
     if user is None:
@@ -56,7 +58,7 @@ def update_profile(uname):
 
     return render_template('profile/update.html',form = form)
 
-@main.route('/user/<uname>/update/pic',methods = ['POST'])
+@main.route('/user/<uname>/update/pic',methods = ['Pitch'])
 @login_required
 def update_pic(uname):
     user = User.query.filter_by(username = uname).first()
@@ -66,3 +68,21 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname = uname))
+
+@main.route('/like/<int:id>', methods=['Pitch', 'GET'])
+@login_required
+def upvote(id):
+    Pitch = Pitch.query.get(id)
+    vote_mpya = Upvote(Pitch=Pitch, upvote=1)
+    vote_mpya.save()
+    return redirect(url_for('main.Pitchs'))
+
+
+@main.route('/dislike/<int:id>', methods=['GET', 'Pitch'])
+@login_required
+def downvote(id):
+    Pitch = Pitch.query.get(id)
+    vm = Downvote(Pitch=Pitch, downvote=1)
+    vm.save()
+    return redirect(url_for('main.Pitchs'))
+    
